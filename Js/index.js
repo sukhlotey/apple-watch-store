dbacd2d4826f8d9dbfb57c686bd53236b11a2fea
SUKHPREET SINGH <82471879+sukhlotey@users.noreply.github.com>
2024-06-14 17:16:08 +0530

    apple watch

--- Js/index.js
+++ Js/index.js
@@ -1,8 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const searchIconImage = document.querySelector('.searchIconImage');
-    const searchInput = document.querySelector('.searchInput');
+document.addEventListener('DOMContentLoaded', function () {
+  const navMenu = document.getElementById('navMenu');
+  const menu = document.querySelector('.menu');
+  const searchIcon = document.querySelector('.searchIconImage');
+  const searchInput = document.querySelector('.search-input');
 
-    searchIconImage.addEventListener('click', () => {
-        searchInput.classList.toggle('visible');
-    });
+  navMenu.addEventListener('click', function () {
+    menu.classList.toggle('active');
+  });
+
+  searchIcon.addEventListener('click', function () {
+    searchInput.classList.toggle('open');
+    if (searchInput.classList.contains('open')) {
+      searchInput.focus();
+    } else {
+      searchInput.blur();
+    }
+  });
 });
